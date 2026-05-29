b066e702094bb179e3f45cbf014df218ffbc18ee
IvanDrv <ivander05@mail.ru>
2026-05-29 21:11:32 +0300

    Feat (database): Entity implemented

---
+++ redirector/src/main/java/com/example/redirector/entity/LinkEntity.java
@@ -0,0 +1,28 @@
+package com.example.redirector.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+@Entity
+@Table(name = "links")
+public class LinkEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "short_link", unique = true, nullable = false)
+    private String shortLink;
+
+    @Column(name = "full_link", unique = false, nullable = false, length = 500)
+    private String fullLink;
+}
