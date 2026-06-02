5e3c876102398d97ac978f0316e647c959dc1107
IvanDrv <ivander05@mail.ru>
2026-06-03 00:45:02 +0300

    Application entry point

---
+++ redirector/src/main/java/com/example/redirector/RedirectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.redirector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectorApplication.class, args);
+	}
+
+}
